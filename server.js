3444b9ed060c9390e281a6e66cbf13b201c17fdf
Arthur Di Lascio <arthur.dilascio@gmail.com>
2019-04-12 19:24:31 -0700

    first commit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const db = require("./config/keys").mongoURI;
 
 mongoose
   .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("MongoDB Connected.."))
+  .then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err));
 
 app.use("/api/items", items);
