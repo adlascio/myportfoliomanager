f882ae5dfa0cc47c7b83e10caaf31dd7bc881703
Arthur Di Lascio <arthur.dilascio@gmail.com>
2019-04-12 21:57:39 -0700

    added transaction list static

---
+++ client/src/components/Portfolio.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import uuid from "uuid";
+
+class Portfolio extends Component {
+  state = {
+    transactions: [
+      { id: uuid(), code: "AAPL", shareQty: 1, sharePrice: 10, type: "Buy" },
+      { id: uuid(), code: "QSR", shareQty: 2, sharePrice: 14, type: "Sell" },
+      { id: uuid(), code: "T", shareQty: 4, sharePrice: 12, type: "Buy" },
+      { id: uuid(), code: "EMA", shareQty: 3, sharePrice: 15, type: "Sell" }
+    ]
+  };
+
+  render() {
+    const { transactions } = this.state;
+    return (
+      <Container>
+        <Button
+          color="dark"
+          style={{ marginBottom: "2rem" }}
+          onClick={() => {
+            const code = prompt("Enter stock code:");
+            const shareQty = prompt("Enter share qty:");
+            const sharePrice = prompt("Enter share price:");
+            const type = prompt("Enter type of transaction:");
+            if (sharePrice && code && shareQty && type) {
+              this.setState(state => ({
+                transactions: [
+                  ...state.transactions,
+                  { id: uuid(), code, shareQty, sharePrice, type }
+                ]
+              }));
+            }
+          }}
+        >
+          Add Transaction
+        </Button>
+        <ListGroup>
+          <TransitionGroup className="transactions-list">
+            {transactions.map(({ id, code }) => (
+              <CSSTransition key={id} timeout={500} classNames="fade">
+                <ListGroupItem>{code}</ListGroupItem>
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </ListGroup>
+      </Container>
+    );
+  }
+}
+
+export default Portfolio;
